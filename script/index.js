f34a9e32f99b3dfe7f51618993957aa0cf327bf9
iiiivanCh <iiiivanchm@yandex.ru>
2022-07-20 16:48:20 +0300

    second fix

--- script/index.js
+++ script/index.js
@@ -2,24 +2,45 @@
 
 const listNew = () => {
   let words = prompt(`del - удаление последней строки\nclear - очистить список\nexit - выход из программы`);
+  if (words !== null) {
+    words = words.trim();
+  }
   console.log(words);
-  if (words === 'exit' || words === null) {
-    return;
+  switch (words) {
+    case 'exit':
+    case null:
+      return;
+    case '':
+      break;
+    case 'del':
+      doItemDel();
+      break;
+    case 'clear':
+      doListDel();
+      break;
+    default:
+      doItemNew(words);
   }
-  words = words.trim();
-  if (words === '') {
-    return listNew();
-  }
-  if (words === 'del') {
-    doItemDel();
-    return listNew();
-  }
-  if (words === 'clear') {
-    doListDel();
-    return listNew();
-  }
-  doItemNew(words);
   return listNew();
+
+
+  // if (words === 'exit' || words === null) {
+  //   return;
+  // }
+  // words = words.trim();
+  // if (words === '') {
+  //   return listNew();
+  // }
+  // if (words === 'del') {
+  //   doItemDel();
+  //   return listNew();
+  // }
+  // if (words === 'clear') {
+  //   doListDel();
+  //   return listNew();
+  // }
+  // doItemNew(words);
+  // return listNew();
 }
 listNew();
 
